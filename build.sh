9bfa98989326b2053acdda93a3d409519bca2eda
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2026-06-21 07:39:18 +0100

    We don't need to pack a tar in our build process

--- build.sh
+++ build.sh
@@ -10,9 +10,3 @@ mkdir build
 echo "* packing KUAL jar"
 PACKED_KUAL_DATA=$(cat src/KUAL.jar | xz -9 | base64 -w 0)
 cat src/KUAL.sh | sed -e "s|PACKED_KUAL_DATA|$PACKED_KUAL_DATA|g" > build/KUAL.sh
-
-echo "* packing tar.gz file"
-tar -czf PEKI.tar.gz -C build .
-rm -rf build/*
-rm -rf build/.*
-mv PEKI.tar.gz build/
